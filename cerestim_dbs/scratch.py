75f7c3b1879c79adb26770277acd1d831743307d
Chadwick Boulay <chadwick.boulay@gmail.com>
2020-10-06 17:17:07 -0400

    Fixup scratch and verify working with stimulator.

--- cerestim_dbs/scratch.py
+++ cerestim_dbs/scratch.py
@@ -6,36 +6,39 @@ import numpy as np
 stimulator = cerestim.BStimulator()
 result, device_tuple = stimulator.scanForDevices()
 stimulator.setDevice(0)
+stimulator.connect(cerestim.BINTERFACE_DEFAULT, None)  # ["003F", 5000, "04D8"])
 max_min = stimulator.getMinMaxAmplitude()
 # should be 16960, 100
 max_amp, min_amp = max_min >> 16, max_min & 0x0F
+min_amp = max(min_amp, 100)
 
-p1_width = 100  # us
-p1_amp = 500
+p1_width = 100  # us; 44-65535
+p1_amp = 500    # 100-10000
 dur = 30
-freq = 300
+freq = 300  # 4-5000
 min_interphase = 53
+max_interphase = 65535
 
 cycle_dur_us = 1e6 / freq
 p2_max_width = cycle_dur_us - p1_width - 2*min_interphase
-p2_amp = (p1_width * p1_amp) / p2_max_width
-# p2_amp = max(p2_amp, min_amp)
+p2_min_amp = (p1_width * p1_amp) / p2_max_width
+p2_amp = int(np.ceil(max(p2_min_amp, min_amp)))
 p2_width = (p1_width * p1_amp) / p2_amp
 
 # interphase = (cycle_dur_us - p1_width - p2_width) / 2  # max interphase
 interphase = min_interphase
-n_pulses = np.ceil(dur * freq)
+n_pulses = int(np.ceil(dur * freq))
 
 if n_pulses > 255:
-    n_reps = np.ceil(n_pulses / 255)
+    n_reps = int(np.ceil(n_pulses / 255))
     n_pulses = 255
 else:
     n_reps = 1
 
 stimulator.configureStimulusPattern(cerestim.BCONFIG_15, cerestim.BWF_ANODIC_FIRST, n_pulses,
-                                    np.uint16(p1_amp), np.uint16(p2_amp),
-                                    np.uint16(p1_width), np.uint16(p2_width),
-                                    np.uint32(freq), np.uint16(interphase))
+                                    p1_amp, p2_amp,
+                                    p1_width, int(np.round(p2_width)),
+                                    freq, interphase)
 # TODO: Add ramp configs 0-14
 
 stimulator.beginningOfSequence()
